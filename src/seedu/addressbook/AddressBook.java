0ca5e94fd0422c16bc53c6231e7f0baaddaf9b94
Guek Ling <yeoyueling@gmail.com>
2018-01-30 22:48:30 +0800

    [W3.2c] Ordering of import statements

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -18,12 +18,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Scanner;
 import java.util.Set;
-import java.util.HashMap;
-import java.util.Comparator;
 
 /*
  * NOTE : =============================================================
